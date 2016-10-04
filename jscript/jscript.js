059eaf00670978222099a0dd1c6a1fd56c8a7a0b
rounaks <rounak.s@zemosolabs.com>
2016-10-04 13:12:15 +0530

    function corrections

--- jscript/jscript.js
+++ jscript/jscript.js
@@ -2,8 +2,9 @@
     var canvas = document.getElementById("myCanvas");
 	var context = canvas.getContext("2d");
 	var points=[];
-	var noPoint=false;
-	var large=0.0;
+	var ZeroPoints=false;
+	var LargestDistance=0.0;
+	
 
 	//function to draw the circle	
 	function draw(e) {
@@ -12,7 +13,7 @@
 	    posy = pos.y;
 	    points.push(pos.x);
 	    points.push(pos.y);
-	    var f=false;
+	    var PointExists=false;
 	    for(var i=0;i<points.length-2;i=i+2){
 	    	if(points[i]===pos.x){
 	    		if(points[i+1]===pos.y){
@@ -24,10 +25,10 @@
 	    		}
 	    	}
 	    }
-	    if(points.length>=4 && noPoint===false){
-	    	noPoint=true;
+	    if(points.length>=4 && ZeroPoints===false){
+	    	ZeroPoints=true;
 	    }
-	    if(f===false){
+	    if(PointExists===false){
 	    	context.fillStyle = "#000000";
 		    context.beginPath();
 		    context.arc(posx, posy, 1, 0, 2*Math.PI);
@@ -41,20 +42,19 @@
 
 	    //Checking for number of points and finding the pair of points at max distance
 	    var dist=[];
-	    if(noPoint===true){
+	    if(ZeroPoints===true){
 			var dictionary = {}
 			for(var i=0;i<points.length-2;i=i+2){
 				for(var j=i+2;j<points.length-1;j=j+2){
 					var len = Math.sqrt(Math.pow((points[j]-points[i]),2)+Math.pow((points[j+1]-points[i+1]),2));
-					//window.alert(len);
 					dictionary[len]=[points[i],points[i+1],points[j],points[j+1]];
 					dist.push(len);
 				}
 			}
-			large=Math.max.apply(null,dist);
+			LargestDistance=Math.max.apply(null,dist);
 			//finding the mid point and drawing circle
-			var midx=(dictionary[large][0]+dictionary[large][2])/2;
-			var midy=(dictionary[large][1]+dictionary[large][3])/2;
+			var midx=(dictionary[LargestDistance][0]+dictionary[LargestDistance][2])/2;
+			var midy=(dictionary[LargestDistance][1]+dictionary[LargestDistance][3])/2;
 			
 			//clearing the canvas of present circle and points
 
@@ -65,22 +65,22 @@
 
 			//drawing the circle
 			context.beginPath();
-			context.arc(midx,midy,large/2,0,2*Math.PI);
+			context.arc(midx,midy,LargestDistance/2,0,2*Math.PI);
 			context.stroke();
-			//if there is a 3r point outside circle finding the centroid and drawing the new circle
+			//if there is a 3r point outside circle finding the circumcenter and drawing the new circle
 			for(var i=0;i<points.length-1;i=i+2){
 				var len = Math.sqrt(Math.pow((midx-points[i]),2)+Math.pow((midy-points[i+1]),2));
-				large=Math.max.apply(null,dist);
-				if(len>large/2){
-					//window.alert("hello");
-					var D = (points[i] - dictionary[large][2]) * (dictionary[large][1] - dictionary[large][3]) - (dictionary[large][0] - dictionary[large][2]) * (points[i+1] - dictionary[large][3]);
-					var circCenterX = (((points[i] - dictionary[large][2]) * (points[i] + dictionary[large][2]) + (points[i+1] - dictionary[large][3]) * (points[i+1] + dictionary[large][3])) / 2 * (dictionary[large][1] - dictionary[large][3])-((dictionary[large][0]-dictionary[large][2]) * (dictionary[large][0] + dictionary[large][2]) + (dictionary[large][1] - dictionary[large][3]) * (dictionary[large][1] + dictionary[large][3])) / 2 * (points[i+1] - dictionary[large][3]))/D;
+				LargestDistance=Math.max.apply(null,dist);
+				if(len>LargestDistance/2){
+					var Circ = circumcenter(dictionary, LargestDistance,i);
 
-					var circCenterY = (((dictionary[large][0]-dictionary[large][2])*(dictionary[large][0]+dictionary[large][2])+(dictionary[large][1]-dictionary[large][3])*(dictionary[large][1]+dictionary[large][3]))/2*(points[i]-dictionary[large][2])-((points[i]-dictionary[large][2])*(points[i]+dictionary[large][2])+(points[i+1]-dictionary[large][3])*(points[i+1]+dictionary[large][3]))/2*(dictionary[large][0]-dictionary[large][2]))/D;
-					var l=Math.sqrt(Math.pow((circCenterX-points[i]),2)+Math.pow((circCenterY-points[i+1]),2));
+					CircX = Circ.CenterX;
+					CircY = Circ.CenterY;
+					
+					var l=Math.sqrt(Math.pow((CircX-points[i]),2)+Math.pow((CircY-points[i+1]),2));
 					context.clearRect(0, 0, canvas.width, canvas.height);
 					context.beginPath();
-					context.arc(circCenterX,circCenterY,l,0,2*Math.PI);
+					context.arc(CircX,CircY,l,0,2*Math.PI);
 		    		context.stroke();
 		    		markPoints();
 					break;
@@ -88,6 +88,14 @@
 				}
 			}
 		}
+	}
+
+//function to get Circumcenter
+	function circumcenter(dict, LargeDist, K){
+		D = (points[K] - dict[LargeDist][2]) * (dict[LargeDist][1] - dict[LargeDist][3]) - (dict[LargeDist][0] - dict[LargeDist][2]) * (points[K+1] - dict[LargeDist][3]);
+					CenterX = (((points[K] - dict[LargeDist][2]) * (points[K] + dict[LargeDist][2]) + (points[K+1] - dict[LargeDist][3]) * (points[K+1] + dict[LargeDist][3])) / 2 * (dict[LargeDist][1] - dict[LargeDist][3])-((dict[LargeDist][0]-dict[LargeDist][2]) * (dict[LargeDist][0] + dict[LargeDist][2]) + (dict[LargeDist][1] - dict[LargeDist][3]) * (dict[LargeDist][1] + dict[LargeDist][3])) / 2 * (points[K+1] - dict[LargeDist][3]))/D;
+
+					CenterY = (((dict[LargeDist][0]-dict[LargeDist][2])*(dict[LargeDist][0]+dict[LargeDist][2])+(dict[LargeDist][1]-dict[LargeDist][3])*(dict[LargeDist][1]+dict[LargeDist][3]))/2*(points[K]-dict[LargeDist][2])-((points[K]-dict[LargeDist][2])*(points[K]+dict[LargeDist][2])+(points[K+1]-dict[LargeDist][3])*(points[K+1]+dict[LargeDist][3]))/2*(dict[LargeDist][0]-dict[LargeDist][2]))/D;
 	}
 //function to mark the points array
 	function markPoints(){
